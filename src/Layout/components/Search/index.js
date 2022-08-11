ba24f4ee19c6419a5e9312020107f94539720244
hoangkyanh <khanhbang3.work@gmail.com>
2022-08-12 03:47:35 +0700

    Sửa lỗi và hoàn thiện phần Header #2

--- src/Layout/components/Search/index.js
+++ src/Layout/components/Search/index.js
@@ -59,42 +59,48 @@ function Search() {
     }, [deBounceValue]);
 
     return (
-        <HeadlessTippy
-            interactive
-            visible={isFocusSearchResult && searchResult.length > 0}
-            render={(attrs) => (
-                <div className={cx('search-result')} tabIndex="-1" {...attrs}>
-                    <PopperWrapper>
-                        <h4 className={cx('search-title')}>Accounts</h4>
-                        {searchResult.map((data) => (
-                            <AccountItem key={data.id} data={data} />
-                        ))}
-                    </PopperWrapper>
-                </div>
-            )}
-            onClickOutside={hanleClickOutSide}
-        >
-            <div className={cx('search')}>
-                <input
-                    ref={inputRef}
-                    value={searchValue}
-                    className={cx('search-input')}
-                    placeholder="Search accounts and videos"
-                    onChange={(e) => handleChange(e.target.value)}
-                    onClick={() => setIsFocusSearchResult(true)}
-                ></input>
-                {searchValue.length > 0 && !loading && (
-                    <FontAwesomeIcon className={cx('search-clear-btn')} icon={faCircleXmark} onClick={handleClear} />
+        <div>
+            <HeadlessTippy
+                interactive
+                visible={isFocusSearchResult && searchResult.length > 0}
+                render={(attrs) => (
+                    <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                        <PopperWrapper>
+                            <h4 className={cx('search-title')}>Accounts</h4>
+                            {searchResult.map((data) => (
+                                <AccountItem key={data.id} data={data} />
+                            ))}
+                        </PopperWrapper>
+                    </div>
                 )}
-                {loading && <FontAwesomeIcon className={cx('search-loading-btn')} icon={faSpinner} />}
-                <button className={cx('search-btn')} onMouseDown={(e) => e.preventDefault()}>
-                    <SearchIcon
-                        fill={searchValue.length > 0 ? 'rgba(22, 24, 35, 0.75)' : 'rgba(22, 24, 35, 0.34)'}
-                        className={cx('search-btn-icon')}
-                    />
-                </button>
-            </div>
-        </HeadlessTippy>
+                onClickOutside={hanleClickOutSide}
+            >
+                <div className={cx('search')}>
+                    <input
+                        ref={inputRef}
+                        value={searchValue}
+                        className={cx('search-input')}
+                        placeholder="Search accounts and videos"
+                        onChange={(e) => handleChange(e.target.value)}
+                        onClick={() => setIsFocusSearchResult(true)}
+                    ></input>
+                    {searchValue.length > 0 && !loading && (
+                        <FontAwesomeIcon
+                            className={cx('search-clear-btn')}
+                            icon={faCircleXmark}
+                            onClick={handleClear}
+                        />
+                    )}
+                    {loading && <FontAwesomeIcon className={cx('search-loading-btn')} icon={faSpinner} />}
+                    <button className={cx('search-btn')} onMouseDown={(e) => e.preventDefault()}>
+                        <SearchIcon
+                            fill={searchValue.length > 0 ? 'rgba(22, 24, 35, 0.75)' : 'rgba(22, 24, 35, 0.34)'}
+                            className={cx('search-btn-icon')}
+                        />
+                    </button>
+                </div>
+            </HeadlessTippy>
+        </div>
     );
 }
 
